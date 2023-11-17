4475423f96e23cead47283230f44ab87a39e8c13
Starz1uz <148357539+Starz1uz@users.noreply.github.com>
2023-11-17 16:51:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,138 @@
+// let password = prompt('password123')
+// if (password === 123) {
+// }else {    
+// }
+
+// let smth = prompt('parol')
+// console.log(smth);
+// alert()
+
+
+// let num = 1.4
+// console.log(Math.ceil(num));
+// let num2 = 1.4
+// console.log(Math.floor(num2));
+// let num3 = 1.5
+// console.log(Math.round(num3));
+
+// let arr = [5,6,3,1]
+// let fn = Math.max(...arr)
+// console.log(fn);
+
+// let sum = 7000
+// if(sum > 6000) {
+// console.log("enter");
+// } else{
+// console.log("error");
+// }
+
+// let a = confirm('are you 18?')
+
+// console.log(a);
+
+// if (a === true) {
+//     alert("enter");
+// } else {
+//     alert("away");
+// }
+// let list = "putin alibek Shavkat biden"
+// let name = prompt('what is your name?').toLowerCase.trin
+// if(list.includes(name)){
+//     console.log('welcome');
+// }else{
+//     console.log('good bye');
+// }
+
+
+//calculator
+// let a = +prompt('a')
+// let m = prompt('+ - / *')
+// let b = +prompt('b')
+
+// if (m === "+") {
+// alert(a + b);
+// } else if (m === "-"){
+// alert(a - b);
+// } else if (m === "/"){
+// alert(a / b);
+// } else if (m === "*") {
+// alert(a * b);
+// } else {
+//     alert('error')
+// }
+
+//
+// let age = prompt('How old are you?')
+// if (1 > age && age <10) {
+//     alert("u are a baby")
+// } else if (11 > age && age < 22) {
+//     alert("u are welcome")
+// } else if(23 > age && age < 33) {
+//     alert("big human")
+// } else if(34 > age && age < 45) {
+//     alert("pensia?")
+// } else {
+//     alert("die")
+// }
+
+// let name = prompt('name')
+// if (name[0] === 'a' && name.length % 2 === 0) {
+//     let password = prompt('age')
+//     if (password == 7777) {
+//         console.log('goood');
+//     }
+// }
+
+
+
+
+
+// homework 1
+// let name = prompt('what is your name')
+// let money = 10000
+
+// if (name === 'Alex') {
+//     console.log("welcome")
+//     let chet = prompt('credit card')
+
+//     if (chet == 7777) {
+//         console.log("nice");
+//         let sale = prompt('сколько обналичить?')
+//         if (sale <= many) {
+//             console.log("everything good");
+//         } else{
+//             console.log('you dont have many');
+//         }
+
+//     }else {
+//         console.log("error");
+//     }
+    
+// } else {
+//     alert('go home')
+// }
+
+// let account = 7777
+
+
+// HOME WORK 2
+
+// let club = prompt('what is your name?').toLowerCase().trim()
+// if (club[0] === 'a') {
+//     let age = prompt('How old are you?').trim()
+//     if (age > 19 && age < 41) {
+//         console.log('welcome');
+//         let money = prompt('how much many you have?')
+//         if (money > 99) {
+//             alert('you are welcome')
+//         } else {
+//             alert('go home never come back')
+//         }
+//     } else {
+//         alert('go home')
+//     }
+    
+// } else {
+//     console.log('go home');
+// }
+// console.log(club);
